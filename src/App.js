34e665ef75b6280f9598263f8eeb4a0fc557a36b
Jarrod Medrano <jmedran@gmail.com>
2020-04-04 14:10:50 -0600

    calculate sums

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const [digits, setDigits] = useState(0);
   const [binaryNum, setBinaryNum] = useState('');
+  const [placeValueMap, setPlaceValueMap] = useState([]);
+  const [binarySum, setBinarySum] = useState(0);
 
   function generateRandomBinary(digits) {
     let binaryString = '';
@@ -15,17 +17,43 @@ function App() {
     setBinaryNum(binaryString);
   }
 
-  function binarytoDec(binaryString) {
-    const splitBinary = binaryString.split('');
+  function mapPlaceValues(string) {
+    const reversed = string.split('').reverse();
+    const placeValueMap = reversed.map((digit, index) => {
+      if(index === 0) return {
+        placeValue: 1,
+        digit: digit
+      } 
+      return {
+        placeValue: Math.pow(2,index),
+        digit: digit
+      }
+    });
 
-    splitBinary.map((digit, index) => {
-      
-    })
+    setPlaceValueMap(placeValueMap);
+  }
+
+  function binaryToDec(placeValueMap) {
+    const sum = placeValueMap.reduce((acc, curr, index) => {
+      if(curr.digit == 0) return acc;
+      return curr.placeValue += acc;
+    }, 0);
+
+    setBinarySum(sum);
   }
 
   useEffect(() => {
     generateRandomBinary(digits);
   }, [digits])
+
+  useEffect(() => {
+    mapPlaceValues(binaryNum);
+  }, [binaryNum]);
+
+  useEffect(() => {
+    binaryToDec(placeValueMap);
+  }, [placeValueMap])
+  
 
   return (
     <div className="App">
@@ -36,6 +64,8 @@ function App() {
       <p>{ 
      binaryNum
       }</p>
+
+    <p>{binarySum}</p>
       <button onClick={() => setDigits(digits - 1)}>
         -
       </button>
