2a2495514ab248e3a486d18b611f6d4e390eb3d5
Jarrod Medrano <jmedran@gmail.com>
2020-04-05 12:54:24 -0600

    Updates

---
+++ precache-manifest.4b5faddcc1cd347fc2de972dfb5c4942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64bcaf3a3430448c5d0b36938449936",
+    "url": "/binary-quiz/index.html"
+  },
+  {
+    "revision": "372402eeefa2bc7d6da4",
+    "url": "/binary-quiz/static/css/main.60868b9f.chunk.css"
+  },
+  {
+    "revision": "6be5b30a0c0b102a2122",
+    "url": "/binary-quiz/static/js/2.640c4fe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/binary-quiz/static/js/2.640c4fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372402eeefa2bc7d6da4",
+    "url": "/binary-quiz/static/js/main.6c98208f.chunk.js"
+  },
+  {
+    "revision": "9532cd20122f052e4ed2",
+    "url": "/binary-quiz/static/js/runtime-main.684dbf63.js"
+  }
+]);
