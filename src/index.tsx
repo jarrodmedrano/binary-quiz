447fe631407000150992bea8aa4626991ce84e8c
Jarrod Medrano <jmedran@gmail.com>
2020-04-17 21:39:52 -0600

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,12 @@
-import { Layout, Menu } from 'antd';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-const { Header, Content, Footer, Sider } = Layout;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Layout className="layout" style={{ height: '100vh' }}>
-      <Sider
-      >
-        <Menu
-          mode="inline"
-          style={{ height: '100%', borderRight: 0 }}
-        >
-          <Menu.Item key="1">option1</Menu.Item>
-          <Menu.Item key="2">option2</Menu.Item>
-          <Menu.Item key="3">option3</Menu.Item>
-          <Menu.Item key="4">option4</Menu.Item>
-        </Menu>
-      </Sider>
-      <Header style={{ background: 'transparent' }} />
-      <Content style={{ padding: '0 50px' }}>
-        <App />
-      </Content>
-      <Footer />
-    </Layout>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
